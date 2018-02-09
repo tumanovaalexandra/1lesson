520248a2a87f5ca3ef23c919c194c622fc3610b6
tumanovaalexandra <35757782+tumanovaalexandra@users.noreply.github.com>
2018-02-09 15:54:20 +0300

    les2_1

--- convertor_automation/features/area_screen.feature
+++ convertor_automation/features/area_screen.feature
@@ -1,31 +1,47 @@
 
-@area
+
 Feature: User is able to convert area units
 
-  @menu
-  Scenario: User is able to dismiss help
-    Given I land on Help popup
-    When I click on Got it button
-    Then I land on Area screen
+  Background:
+    Given I click on Got it button
+    And I land on Area screen
 
-@wip
-   Scenario: User is able to swap values
-     Given I click on Got it button
-     And I see "Sq Kilometre" in From header
+
+ # Scenario: User is able to dismiss help
+  #  Given I land on Help popup
+   # When I click on Got it button
+    #Then I land on Area screen
+
+  Scenario: User is able to swap values
+     Given I see "Sq Kilometre" in From header
      And I see "Sq Metre" in To header
      When I click on Swap button
      Then I see "Sq Metre" in From header
      And I see "Sq Kilometre" in To header
 
-     Scenario Outline: User is able to convert default unite
-       Given I click on Got it button
-       And I click on Clear button
+
+   Scenario Outline: User is able to convert default unite
+       Given I click on Clear button
        When I enter "<target>" to From field
-       Then I get "<result>" in To failed
+       Then I get "<result>" in To field
        Examples:
        |target|result|
        |1    |1000000|
        |2    |2000000|
        |3    |3000000|
+
+   Scenario:
+     Given I click on Clear button
+     When I click on From field
+     And I press "12" on soft keyboard
+     Then I get "12000000" in To field
+
+
+  @wip
+   Scenario:
+    When I select "Hectare" from left column
+    Then I see "Hectare" in From header
+    And I get "10000" in To field
+
 
 
--- convertor_automation/features/step_definitions/area screen steps.rb
+++ convertor_automation/features/step_definitions/area screen steps.rb
@@ -1,35 +1,60 @@
 Given(/^I land on Help popup$/) do
-  puts ("User lands on Help popup")
+  text ("Help")
 end
 
 When(/^I click on Got it button$/) do
-  puts ("User click on Got in button")
+   find_element(id: "button1").click
 end
 
 Then(/^I land on Area screen$/) do
-  puts ("User lands on Area screen")
+  text ("Area")
 end
 
 When(/^I click on Swap button$/) do
-  puts ("User clicks Swap button")
+ find_element(id: "fab").click
 end
 
 Then(/^I see "([^"]*)" in From header$/) do |value|
-  puts ("From header values is "+ value)
+  actual_value = find_element(id: "header_text_unit_from").text
+  puts("Expected value is #{value}")
+  puts("Actual value is #{actual_value}")
 end
 
 And(/^I see "([^"]*)" in To header$/) do |value|
-  puts ("To header values is #{value}")
-end
+  actual_value = find_element(id: "header_text_unit_to").text
+  puts("Expected value is #{value}")
+  puts("Actual value is #{actual_value}")end
 
 And(/^I click on Clear button$/) do
-  puts ("User clicks on Clear button")
+  find_element(id: "menu_clear").click
 end
 
 When(/^I enter "([^"]*)" to From field$/) do |value|
-  puts("User entered value #{value}")
+  find_element(id: "header_value_from").send_keys(value)
 end
 
-Then(/^I get "([^"]*)" in To failed$/) do |value|
-  puts ("User sees #{value} in result field")
+Then(/^I get "([^"]*)" in To field$/) do |value|
+  actual_value = find_element(id: "header_value_to").text
+  #puts("Expected value is #{value}")
+ # puts("Actual value is #{actual_value}")
+  if actual_value != value
+    fail("Expected value is #{value}, but actual value was #{actual_value}")
+  end
+end
+
+When(/^I click on From field$/) do
+  find_element(id: "header_value_from").click
+end
+
+And(/^I press "([^"]*)" on soft keyboard$/) do |value|
+  digits = value.split("")
+  digits.each do|key|
+     digit = Integer(key)
+     press_keycode 7 + digit
+  end
+end
+
+
+When(/^I select "([^"]*)" from left column$/) do |value|
+  find_element(id: "radio_group_from").find_element(xpath: "//android.widget.RadioButton[@text='#{value}']").click
 end
